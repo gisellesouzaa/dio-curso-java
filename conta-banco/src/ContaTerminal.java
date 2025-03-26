7457585de491fcaa67541a097c1d10246b1e1e03
Giselle Souza <giselle_souza97@yahoo.com.br>
2025-03-26 16:44:26 -0300

    commit challenge

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        
+        try {
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da conta!");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o saldo");
+        double saldo = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Digite o seu nome completo!");
+        String nomeCliente = scanner.nextLine();
+        
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, ");
+        System.out.println("sua agência é [" + agencia + "], conta [" + numero + "] e seu saldo [R$ " + String.format("%.2f", saldo) + "] já está disponível para saque.");
+        scanner.close();
+
+        } catch (Exception e) {
+            System.out.println("Verifique o tipo de dado digitado e tente novamente.");
+        }
     }
 }
